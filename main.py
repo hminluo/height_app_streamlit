f0d522152b99860569890cc48c1daa9d383cb969
hminluo <hminluo921128@gmail.com>
2020-10-16 22:55:12 -0400

    added banana measurement

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ def main():
 	st.text('This tool uses advanced technology to measure your height based on a few simple questions')
 	st.text_input('What is your zip code', value='90210')
 	height = st.text_input('How tall are you in inches?', value='60')
+	bananas = round(float(height)/7.0, 2)
 	
 
 	if st.button('Measure Me'):
-		st.text('You are '+ height + ' inches tall. That is about '+ height/7 + ' bananas long!')
+		st.text('You are '+ height + ' inches tall. That is about '+ str(bananas) + ' bananas long!')
 		st.text('Not satisfied with your result?')
 		st.text('Please make sure the information you provided is accurate, and try again!')
 
